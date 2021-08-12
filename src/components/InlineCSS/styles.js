0e0911d3cdb9a62065c7fc72d69d78fd7717a57f
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-08-13 02:25:11 +0300

    Add style js for inline js component

---
+++ src/components/InlineCSS/styles.js
@@ -0,0 +1,8 @@
+export const inlineStyle = {
+    div: {
+        backgroundColor: "yellowgreen",
+        padding: "2rem",
+        textAlign: "center",
+    },
+    para: { color: "white", fontWeight: "bold" },
+};

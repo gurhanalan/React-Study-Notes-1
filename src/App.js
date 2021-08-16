99d6b02e0434f55d41be8f2f4af37f29119cedab
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-08-16 14:18:18 +0300

    Add Module Css

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import StyleInlineCss from "./components/InlineCSS/StyleInlineCss";
 import StyleSheet from "./components/regular-style/StyleSheet";
 import SassyStyle from "./components/Sassy/SassyStyle";
 
+import "./app.css";
+import styles from "./app.module.css";
+
 function App() {
     return (
-        <div className="App">
+        <div>
             <ClassComponent name="Jane Doe" />
             <TitleFunctionalComp name="John Doe" />
 
             <StyleInlineCss />
             <StyleSheet primary={false} />
             <SassyStyle />
+
+            <h3 className="error">General Style Error</h3>
+            <h3 className={styles.success}>Module Style Success</h3>
+            <h3 className={styles["success__BEM__notation--1"]}>
+                Module Style Success with Bracket Notation
+            </h3>
         </div>
     );
 }
